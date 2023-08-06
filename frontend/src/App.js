85828accd04b35cf1be5ac8496a2de463d6aaaee
Michael Leong <michaelleong1229@gmail.com>
2023-08-06 11:18:10 -0700

    fix routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,45 +1,41 @@
 import React, { Suspense } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import CssBaseline from "@mui/material/CssBaseline";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { createTheme, ThemeProvider } from "@mui/material";
 
-function App() {
-  const Home = React.lazy(() => import("./Home.js"));
-  const NotFound = React.lazy(() => import("./NotFound.js"));
-  const Calculator = React.lazy(() => import("./Calculator.js"));
+import Home from "./components/Home.js";
+import NotFound from "./components/NotFound.js";
+import Calculator from "./components/Calculator.js";
 
-  const theme = createTheme({
-    palette: {
-      primary: {
-        light: "#fffff", // White
-        main: "#ffffff", // White
-        dark: "#BFBFBF", // Dark Gray
-        contrastText: "#00000", // Black
-      },
-      secondary: {
-        light: "#d5f0f7", // Light Blue
-        main: "#5d94c4", // Middle Blue
-        dark: "#06075c", // Navy
-        contrastText: "#ffffff",
-      },
-      mode: "light",
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: "#fffff", // White
+      main: "#ffffff", // White
+      dark: "#BFBFBF", // Dark Gray
+      contrastText: "#00000", // Black
     },
-  });
+    secondary: {
+      light: "#d5f0f7", // Light Blue
+      main: "#5d94c4", // Middle Blue
+      dark: "#06075c", // Navy
+      contrastText: "#ffffff",
+    },
+    mode: "light",
+  },
+});
 
+export default function App() {
   return (
     <ThemeProvider theme={theme}>
-      <CssBaseline />
       <BrowserRouter>
-        <Suspense>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/calculator" element={<Calculator />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </Suspense>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/calculator" element={<Calculator />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
       </BrowserRouter>
     </ThemeProvider>
+
   );
 }
-
-export default App;
