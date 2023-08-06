987203a17c90c25d5b3cf2697e58f56d89249ab1
Michael Leong <michaelleong1229@gmail.com>
2023-08-06 12:15:44 -0700

    add autofill checkbox

--- frontend/src/components/Calculator.js
+++ frontend/src/components/Calculator.js
@@ -7,23 +7,48 @@ import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import styled from '@mui/material/styles/styled';
+import { useState } from "react";
+import IconButton from "@mui/material/IconButton";
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+import Autocomplete from '@mui/material/Autocomplete';
+import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
+import CheckBoxIcon from '@mui/icons-material/CheckBox';
+import Checkbox from '@mui/material/Checkbox';
+
 
 
 const logoPath = "frontend/public/rocket_logo_full.png";
+const icon = <CheckBoxOutlineBlankIcon fontSize="small" />;
+const checkedIcon = <CheckBoxIcon fontSize="small" />;
+
 
 //for testing grid visualization
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: 'center',
-  color: theme.palette.text.secondary,
-}));
+// const Item = styled(Paper)(({ theme }) => ({
+//   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+//   ...theme.typography.body2,
+//   padding: theme.spacing(1),
+//   textAlign: 'center',
+//   color: theme.palette.text.secondary,
+// }));
+
+const top100Films = [
+  { title: 'The Shawshank Redemption', year: 1994 },
+  { title: 'The Godfather', year: 1972 },
+  { title: 'The Godfather: Part II', year: 1974 },
+  { title: 'The Dark Knight', year: 2008 },
+  { title: '12 Angry Men', year: 1957 },
+  { title: "Schindler's List", year: 1993 },
+  { title: 'Pulp Fiction', year: 1994 },
+]
 
 
-function Calculator() {
+
+export default function Calculator() {
+
+
   return (
-    <Box sx={{ flexGrow: 1 }}>
+    <Box sx={{ flexGrow: 1, marginTop: 2 }}>
       <Grid container spacing={1}>
         <Grid item md={6} xs={12}>
           <FullCalendar
@@ -41,7 +66,31 @@ function Calculator() {
           />
         </Grid>
         <Grid item md={6} xs={12}>
-          sheesh
+          <Autocomplete
+            multiple
+            id="checkboxes-tags-demo"
+            options={top100Films}
+            disableCloseOnSelect
+            getOptionLabel={(option) => option.title}
+            renderOption={(props, option, { selected }) => (
+              <li {...props}>
+                <Checkbox
+                  icon={icon}
+                  checkedIcon={checkedIcon}
+                  style={{ marginRight: 8 }}
+                  checked={selected}
+                />
+                {option.title}
+              </li>
+
+            )}
+            style={{ width: 500 }}
+            renderInput={(params) => (
+              <TextField {...params} label="Checkboxes" placeholder="Favorites" />
+
+            )}
+          />
+
         </Grid>
       </Grid>
     </Box>
@@ -51,4 +100,3 @@ function Calculator() {
   );
 }
 
-export default Calculator;
