fa1af50c7b2b6519749243759afb0d4ea8fd8d1c
aboombadev <abhayshuklayt@gmail.com>
2024-08-08 20:29:00 -0700

    icon button

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,42 @@
 import * as React from "react";
 import { Suspense } from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
-import { createTheme, ThemeProvider } from "@mui/material";
+import { createTheme, styled, ThemeProvider } from "@mui/material";
 
 import Home from "./pages/Home.tsx";
 import NotFound from "./components/NotFound.tsx";
 import Scheduler from "./pages/Scheduler.tsx";
-import AppBar from "./components/SiteAppBar.tsx";
+import InfoBubble from "./components/InfoBubble.tsx";
 
 const theme = createTheme({
   palette: {
     primary: {
-      light: "#fffff", // White
-      main: "#06075c", // White
-      dark: "#BFBFBF", // Dark Gray
-      contrastText: "#00000", // Black
+      light: "#d5f0f7",
+      main: "#5d94d4",
+      dark: "#06075c",
+      contrastText: "#ffffff", // White
     },
     secondary: {
-      light: "#d5f0f7", // Light Blue
-      main: "#06075c", // Navy
-      dark: "#5d94c4", // Middle Blue
-      contrastText: "#ffffff",
+      light: "#ffffff",
+      main: "#ffffff",
+      dark: "#ffffff",
+      contrastText: "#000000", // Black
     },
     mode: "light",
   },
+});
+
+const InfoBubbleContainer = styled("div")({
+  position: "absolute",
+  bottom: 16,
+  right: 16,
 });
 
 export default function App({}) {
   return (
     <ThemeProvider theme={theme}>
       <BrowserRouter>
-        <AppBar />
+        <InfoBubbleContainer />
         <Suspense>
           <Routes>
             <Route path="/" element={<Home />} />
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,59 +3,54 @@ import { useNavigate } from "react-router-dom";
 import Grid from "@mui/material/Grid";
 import { Typography } from "@mui/material";
 import * as React from "react";
+import InfoBubble from "../components/InfoBubble";
 
 export default function Home() {
   let navigate = useNavigate();
 
   return (
     <>
-    <Grid
-      container
-      spacing={0}
-      direction="column"
-      alignItems="center"
-      justifyContent="center"
-      alignContent="center"
-    >
-      <Grid item xs={12} sx={{ marginTop: 40 }}>
-        <Typography
-          variant="h1"
-          color="common.white"
-          sx={{ fontWeight: "bold" }}
-          align="center"
-        >
-          VelocityDraft
-        </Typography>
-        <Typography
-          align="center"
-          variant="h4"
-          color="secondary.light"
-          sx={{
-            fontWeight: 400,
-            alignContent: "center",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          College Application Essay Scheduler
-        </Typography>
-      </Grid>
-      <Button
-        variant="contained"
-        sx={{
-          color: "white",
-          fontWeight: "medium",
-          marginTop: 1,
-          width: 550,
-          height: 50,
-          marginBottom: 49,
-          fontSize: "20px",
-        }}
-        onClick={() => navigate("/scheduler")}
+      <Grid
+        container
+        spacing={0}
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+        alignContent="center"
       >
-        Try it out! It's free!
-      </Button>
-    </Grid>
+        <Grid item xs={12} sx={{ marginTop: 40 }}>
+          <Typography variant="h1" sx={{ fontWeight: "bold" }} align="center">
+            VelocityDraft
+          </Typography>
+          <Typography
+            align="center"
+            variant="h4"
+            sx={{
+              fontWeight: 400,
+              alignContent: "center",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            College Application Essay Scheduler
+          </Typography>
+        </Grid>
+        <Button
+          variant="contained"
+          sx={{
+            color: "white",
+            fontWeight: "medium",
+            marginTop: 1,
+            width: 550,
+            height: 50,
+            marginBottom: 49,
+            fontSize: "20px",
+          }}
+          onClick={() => navigate("/scheduler")}
+        >
+          Try it out! It's free!
+        </Button>
+      </Grid>
     </>
   );
 }
