a01d31400220c0ce3a351f21005db71d462e3053
aboombadev <abhayshuklayt@gmail.com>
2024-08-09 21:01:47 -0700

    fix ios bug, more scaling fixes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,7 @@
 import Button from "@mui/material/Button";
 import { useNavigate } from "react-router-dom";
 import Grid from "@mui/material/Grid";
-import {
-  CircularProgress,
-  Divider,
-  IconButton,
-  Stack,
-  TextField,
-  Typography,
-  useMediaQuery,
-  useTheme,
-} from "@mui/material";
+import { Divider, Stack, TextField, Typography, useTheme } from "@mui/material";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 import { TypeAnimation } from "react-type-animation";
 import { useEffect, useRef, useState } from "react";
@@ -36,12 +27,14 @@ function Home() {
         return maxWidth / elementWidth;
       };
 
-      const elementWidth = titleStackRef.current.offsetWidth;
-      const windowWidth = window.innerWidth;
-      const calculatedScale = calculateScale(windowWidth, elementWidth);
+      if (titleStackRef.current !== null) {
+        const elementWidth = titleStackRef.current.offsetWidth;
+        const windowWidth = window.innerWidth;
+        const calculatedScale = calculateScale(windowWidth, elementWidth);
 
-      console.log(elementWidth, windowWidth, calculatedScale);
-      setScaleFactor(calculatedScale);
+        console.log(elementWidth, windowWidth, calculatedScale);
+        setScaleFactor(calculatedScale);
+      }
     };
 
     window.addEventListener("resize", handleResize);
@@ -51,117 +44,116 @@ function Home() {
   }, []);
 
   return (
-    <>
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      alignContent="center"
+      sx={{
+        height: "100svh",
+        display: "grid",
+        paddingBottom: "1px",
+      }}
+    >
       <Grid
-        container
-        spacing={0}
-        direction="column"
-        alignItems="center"
-        justifyContent="center"
-        alignContent="center"
+        item
         sx={{
-          height: "100vh",
-          display: "grid",
-          gridTemplateRows: "80% 50%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          overflow: "hidden",
+          py: 15,
         }}
       >
-        <Grid
-          item
+        <Stack
+          ref={titleStackRef}
+          direction="row"
+          spacing={2}
           sx={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            overflow: "hidden",
+            paddingRight: 2,
+            transform: scaleFactor < 1 ? `scale(${scaleFactor})` : "scale(1)",
           }}
         >
-          <Stack
-            ref={titleStackRef}
-            direction="row"
-            spacing={2}
+          <Divider
+            orientation="vertical"
+            flexItem
             sx={{
-              paddingRight: 2,
-              transform: scaleFactor < 1 ? `scale(${scaleFactor})` : "scale(1)",
+              borderRightWidth: 5,
+              borderRadius: 5,
             }}
-          >
-            <Divider
-              orientation="vertical"
-              flexItem
+          />
+
+          <Stack>
+            <Typography
+              variant="h1"
               sx={{
-                borderRightWidth: 5,
-                borderRadius: 5,
+                color: theme.palette.primary.main,
+                fontWeight: "bold",
+                textAlign: "left",
               }}
-            />
-
-            <Stack>
-              <Typography
-                variant="h1"
-                sx={{
-                  color: theme.palette.primary.main,
-                  fontWeight: "bold",
-                  textAlign: "left",
-                }}
-              >
-                VelocityDraft
-              </Typography>
-              <Typography
-                sx={{
-                  fontSize: 33,
-                  fontWeight: 400,
-                  textAlign: "left",
-                }}
-              >
-                <TypeAnimation
-                  sequence={["Flexible Application Essay Scheduler"]}
-                  speed={65}
-                  style={{
-                    color: theme.palette.secondary.contrastText,
-                    fontSize: 33,
-                    fontWeight: 500,
-                    paddingLeft: 7,
-                    alignContent: "left",
-                    alignItems: "left",
-                    whiteSpace: "nowrap",
-                  }}
-                />
-              </Typography>
-            </Stack>
-          </Stack>
-        </Grid>
-
-        <Grid item sx={{ padding: 3, width: "100%" }}>
-          <Stack direction="row" spacing={1}>
-            <TextField
-              color="primary"
-              label="Enter your name here..."
-              fullWidth
-              onChange={(e) => {
-                setName(e.target.value);
-              }}
-              InputLabelProps={{
-                shrink: false,
-                style: {
-                  marginTop: "1px",
-                  opacity: isNameEntered ? 0 : 1,
-                },
-              }}
-              InputProps={{
-                style: {
-                  borderRadius: 40,
-                },
-              }}
-            />
-            <Button
-              variant="contained"
-              disabled={!isNameEntered}
-              sx={{ borderRadius: 40, minWidth: 0, aspectRatio: 1 / 1 }}
-              onClick={() => navigate("/scheduler")}
             >
-              <ArrowForwardIcon />
-            </Button>
+              VelocityDraft
+            </Typography>
+            <Typography
+              sx={{
+                fontSize: 33,
+                fontWeight: 400,
+                textAlign: "left",
+              }}
+            >
+              <TypeAnimation
+                sequence={["Flexible Application Essay Scheduler"]}
+                speed={65}
+                style={{
+                  color: theme.palette.secondary.contrastText,
+                  fontSize: 33,
+                  fontWeight: 500,
+                  paddingLeft: 7,
+                  alignContent: "left",
+                  alignItems: "left",
+                  whiteSpace: "nowrap",
+                }}
+              />
+            </Typography>
           </Stack>
-        </Grid>
+        </Stack>
       </Grid>
-    </>
+
+      <Grid item sx={{ padding: 3, width: "100%", paddingTop: 35 }}>
+        <Stack direction="row" spacing={1}>
+          <TextField
+            color="primary"
+            label="Enter your name here..."
+            fullWidth
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+            InputLabelProps={{
+              shrink: false,
+              style: {
+                marginTop: "1px",
+                opacity: isNameEntered ? 0 : 1,
+              },
+            }}
+            InputProps={{
+              style: {
+                borderRadius: 40,
+              },
+            }}
+          />
+          <Button
+            variant="contained"
+            disabled={!isNameEntered}
+            sx={{ borderRadius: 40, minWidth: 60 }}
+            onClick={() => navigate("/scheduler")}
+          >
+            <ArrowForwardIcon />
+          </Button>
+        </Stack>
+      </Grid>
+    </Grid>
   );
 }
 
