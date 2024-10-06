1b70aced89ae5b0b4e25c650383d8a518980b47e
Abhay Shukla <105139789+shuklabhay@users.noreply.github.com>
2024-10-06 16:26:08 -0700

    try to fix routing issue

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default () => {
-  return defineConfig({
-    plugins: [react()],
-    base: "/velocity-draft/",
-    404: "./404.html",
-  });
-};
+export default defineConfig({
+  plugins: [
+    react(),
+    {
+      name: "copy-404",
+      closeBundle: () => {
+        fs.copyFileSync("404.html", "dist/404.html");
+      },
+    },
+  ],
+  base: "/velocity-draft/",
+});
