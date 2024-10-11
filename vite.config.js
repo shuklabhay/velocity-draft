8ec502f6309f556e435264de41e95e3418a73434
aboombadev <abhayshuklayt@gmail.com>
2024-10-11 00:15:55 -0700

    move 404.html into public

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,6 @@ import react from "@vitejs/plugin-react";
 import fs from "fs";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    {
-      name: "copy-static-deps",
-      closeBundle: () => {
-        fs.copyFileSync("404.html", "dist/404.html");
-      },
-    },
-  ],
+  plugins: [react()],
   base: "/velocity-draft/",
 });
